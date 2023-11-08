1ff5dd1b96aa74fc56e226f6bb572f51e9e2b09e
LuxuryDuty <150188177+LuxuryDuty@users.noreply.github.com>
2023-11-08 17:22:56 +0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import json
-import os
+import os, sys
+import platform
 
 
 def get_token(text):
@@ -9,28 +10,65 @@ def get_token(text):
     except:
         return text
 
+
 def setup():
-    try:
-        os.mkdir('luxurylp')
-    except:
-        pass
-    
-    tokens = []
-    while len(tokens) != 3:
-        token = input("Введите токен VK не обрезая его >> ")
+    if sys.version_info < (3, 7) or sys.version_info > (3, 8):
+        print("Версия Python не соответствует требованиям")
+        exit(1)
+    else:
+        print("Версия Python соответствует требованиям")
+    os_name = platform.system()
+    if os_name == "Linux":
+        print("Установка на Linux.")
+        os.system("sudo apt update")
+        os.system("sudo apt install apt-transport-https ca-certificates curl software-properties-common")
+        os.system("curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -")
+        os.system("sudo add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\"")
+        os.system("sudo apt update")
+        os.system("sudo apt install docker-ce")
+        
+        os.system(f'{sys.executable} -m pip install --upgrade pip')
+        os.system(f'{sys.executable} -m pip install -r requirements.txt')
 
-        tokens.append(get_token(token))
+        tokens = []
+        while len(tokens) != 3:
+            token = input("Введите токен VK не обрезая его >> ")
 
-    with open(os.path.join('config.json'), 'w', encoding='utf-8') as file:
-        db = {
-            "tokens": tokens
-        }
-        file.write(json.dumps(db))
+            tokens.append(get_token(token))
 
-    with open(os.path.join('luxurylp', 'lp_dc_config.json'), 'w', encoding='utf-8') as file:
-        file.write('{"app_secret": "public", "app_id": 0}')
+        with open(os.path.join('config.json'), 'w', encoding='utf-8') as file:
+            db = {
+                "tokens": tokens
+            }
+            file.write(json.dumps(db))
 
-    print("Конфиг записан")
+        with open(os.path.join('luxurylp', 'lp_dc_config.json'), 'w', encoding='utf-8') as file:
+            file.write('{"app_secret": "public", "app_id": 0}')
+
+        print("Конфиг записан")
+    elif os_name == "Windows":
+        print("Установка на Windows.")
+
+        os.system(f'{sys.executable} -m pip install --upgrade pip')
+        os.system(f'{sys.executable} -m pip install -r requirements.txt')
+
+        tokens = []
+        while len(tokens) != 3:
+            token = input("Введите токен VK не обрезая его >> ")
+
+            tokens.append(get_token(token))
+
+        with open(os.path.join('config.json'), 'w', encoding='utf-8') as file:
+            db = {
+                "tokens": tokens
+            }
+            file.write(json.dumps(db))
+
+        with open(os.path.join('luxurylp', 'lp_dc_config.json'), 'w', encoding='utf-8') as file:
+            file.write('{"app_secret": "public", "app_id": 0}')
+
+        print("Конфиг записан")
+
 
 if __name__ == '__main__':
     setup()
